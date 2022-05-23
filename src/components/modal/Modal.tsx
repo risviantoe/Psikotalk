0960012bd08d8d6a19be7f13295bb0beb4d7208f
Edwin Risvianto <risviantoe@gmail.com>
2022-05-23 15:12:11 +0800

    modal detail & confirmation done

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import Button from '../button/Button'
 
 import './Modal.css'
 
 interface ModalProps {
-
+	show: boolean
+	closeButton?: boolean
+	onClose?: () => void
 }
 
-const Modal = () => {
+const Modal: React.FC<ModalProps> = ({
+	children,
+	closeButton = true,
+	show = false,
+	onClose
+}) => {
+
+	if (!show) {
+		return null
+	}
+	
 	return (
 		<div className="modal">
 			<div className="modal-content">
-				<h1>Modal</h1>
+				{
+					closeButton ? 
+						<Button
+							color="primary"
+							icon="icon-cross"
+							iconSize={15}
+							type="circle"
+							size="small"
+							style={{ position: 'absolute', right: -28, top: -30, border: '4px solid #fff', height: 60, width: 60, display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+							onClick={onClose}
+						/> : null
+				}
+				
+				{ children }
 			</div>
 		</div>
-	)
-};
+	);
+}
 
 export default Modal
+
--- src/components/scheduleList/ScheduleList.tsx
+++ src/components/scheduleList/ScheduleList.tsx
@@ -1,27 +1,32 @@
-import React, { useState } from "react";
-import Button from "../button/Button";
-import Modal from "../modal/Modal"
+import React, { useState } from 'react';
+import Button from '../button/Button';
+import Modal from '../modal/Modal';
+import ModalConfirmation from '../modalConfirmation/ModalConfirmation';
+import ModalScheduleDetail from '../modalSchedule/ModalScheduleDetail';
 
-import "./ScheduleList.css";
+import './ScheduleList.css';
 
 interface ScheduleListProps {
-    day: string;
-    date: string;
-    name: string;
-    clock: string;
-    type: string;
+	day: string;
+	date: string;
+	name: string;
+	clock: string;
+	type: string;
+	confirm?: boolean;
 }
 
 const ScheduleList: React.FC<ScheduleListProps> = ({
-    day = "Senin",
-    date = "11 Maret",
-    name = "Johnathan Alex",
-    clock = "07.00 - 09.00 WITA",
-    type = "schedule",
+	day,
+	date,
+	name,
+	clock,
+	type,
+	confirm = false,
 }) => {
-    const [showModal, setShowModal] = useState(false)
+	const [showModalDetail, setShowModalDetail] = useState(false);
+	const [showModalConfirm, setShowModalConfirm] = useState(false);
 
-    return (
+	return (
 		<React.Fragment>
 			<div className="psikolog-schedule-item">
 				<div
@@ -44,15 +49,27 @@ const ScheduleList: React.FC<ScheduleListProps> = ({
 						{type === 'schedule' ? (
 							<>
 								<Button
-									name="Dikonfirmasi"
-									style={{
-										borderRadius: 20,
-										padding: '10px 14px',
-										width: 140,
-									}}
+									name={confirm ? 'Batalkan' : 'Konfirmasi'}
+									onClick={() => setShowModalConfirm(true)}
+									style={
+										confirm
+											? {
+													borderRadius: 20,
+													padding: '10px 14px',
+													width: 140,
+													background: '#7E808B',
+													border: '1px solid #7E808B',
+											  }
+											: {
+													borderRadius: 20,
+													padding: '10px 14px',
+													width: 140,
+											  }
+									}
 								/>
 								<Button
 									name="Detail"
+									onClick={() => setShowModalDetail(true)}
 									style={{
 										borderRadius: 20,
 										padding: '10px 14px',
@@ -63,7 +80,7 @@ const ScheduleList: React.FC<ScheduleListProps> = ({
 						) : (
 							<Button
 								name="Detail"
-								onClick={() => setShowModal(true)}
+								onClick={() => setShowModalDetail(true)}
 								style={{
 									borderRadius: 20,
 									padding: '10px 14px',
@@ -73,8 +90,32 @@ const ScheduleList: React.FC<ScheduleListProps> = ({
 						)}
 					</div>
 				</div>
-            </div>
-            <Modal />
+			</div>
+			<Modal show={showModalConfirm} closeButton={false}>
+				<ModalConfirmation
+					text={
+						confirm
+							? 'Anda yakin membatalkan Jadwal Konsultasi?'
+							: 'Konfirmasi Jadwal Konsultasi dengan Klien'
+					}
+					confirm={confirm}
+					onClose={() => setShowModalConfirm(false)}
+				/>
+			</Modal>
+
+			<Modal
+				onClose={() => setShowModalDetail(false)}
+				show={showModalDetail}
+			>
+				<ModalScheduleDetail
+					name="M Chandra"
+					gender="Pria"
+					date="Senin, 16 April 2022"
+					clock="09.00 - 10.00 WITA"
+					platform="Zoom"
+					link="https://zoom.us/j/9069558658?pwd=SmtJTnlibGtNQmg3aUhvODRFZTBNdz09"
+				/>
+			</Modal>
 		</React.Fragment>
 	);
 };
